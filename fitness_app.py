58709ab7a2250d8cff0a1553a608d483b33dffa1
gitofashish <75847197+gitofashish@users.noreply.github.com>
2024-02-04 14:13:37 +0530

    fitness_app.py

--- fitness_app.py
+++ fitness_app.py
@@ -1,7 +1,7 @@
 from langchain.llms import OpenAI
 import os
 
-os.environ['OPENAI_API_KEY'] = "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"
+os.environ['OPENAI_API_KEY'] = "**********************"
 llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
 
 text = "Suggest a personalized workout routine for someone looking to improve cardiovascular endurance and prefers outdoor activities."
